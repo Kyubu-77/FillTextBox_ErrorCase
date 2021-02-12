fecd96e698bf6e97d0b80009133fff6e680eb7bf
Kyubu-77 <kobler@fbihome.de>
2021-02-13 00:33:30 +0100

    Init

---
+++ Program.vb
@@ -0,0 +1,54 @@
+Imports System
+Imports System.Linq
+Imports GemBox.Pdf
+Imports GemBox.Pdf.Forms
+
+
+Module Program
+
+    Sub Main()
+        Console.WriteLine("Hello World!")
+
+        ' If using Professional version, put your serial key below.
+        ComponentInfo.SetLicense("FREE-LIMITED-KEY")
+
+        Using document = PdfDocument.Load("..\..\..\Form.pdf")
+
+            ' Set check box field checked state.
+            Dim marriedCheckBox = CType(document.Form.Fields("Married"), PdfCheckBoxField)
+            marriedCheckBox.Checked = True
+
+            'NEW------System.TypeInitializationException
+            Dim i As Integer = 0
+            For Each field As PdfField In document.Form.Fields
+
+                If TypeOf field Is PdfTextField Then
+                    Dim textField As PdfTextField = field
+                    textField.Value = textField.Name & "_" & i
+                    i = i + 1
+                End If
+            Next
+
+
+
+            ' Set check box field value.
+            Dim drivingLicenseCheckBox = CType(document.Form.Fields("Driving License"), PdfCheckBoxField)
+            drivingLicenseCheckBox.Value = "Yes"
+
+            ' Set radio button field checked state.
+            ' There are multiple radio button fields with name 'Gender' so
+            ' set checked state only to the radio button field whose choice is 'Male'.
+            For Each genderRadioButton As PdfRadioButtonField In document.Form.Fields.Where(Function(field) field.Name = "Gender")
+                genderRadioButton.Checked = genderRadioButton.Choice = "Male"
+            Next
+
+            ' Set radio button field value.
+            ' It is enough to set value to only one radio button field 
+            ' from a group of radio button fields with name 'Age'.
+            Dim ageRadioButton = CType(document.Form.Fields("Age"), PdfRadioButtonField)
+            ageRadioButton.Value = "18 - 39"
+
+            document.Save("Fill in Form.pdf")
+        End Using
+    End Sub
+End Module
